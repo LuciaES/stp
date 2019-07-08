e7194c89f0195838c7ca5787d98d6bbe0cf852e4
Lucia Espiga <Lucia.espiga@gmail.com>
2019-07-09 00:56:22 +0200

    index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'contact', to:'pages#contact'
   get 'home', to: 'pages#home'
 
-  root to: 'pages#index'
+  root :to=> 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
