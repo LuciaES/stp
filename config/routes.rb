fc874a0726b87046808e866ece6e9296af4e6225
Lucia Espiga <Lucia.espiga@gmail.com>
2019-08-06 02:03:58 +0200

    new pages, files changed and new code layour

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  get 'about', to:'pages#about'
-  get 'contact', to:'pages#contact'
-
-  root :to => 'pages#index'
-  # root :to=> 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
